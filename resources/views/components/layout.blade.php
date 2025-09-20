1872df3f6b7946d7c27d9ec336cc5b475a13b83f
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-09-20 11:46:34 +0530

    feat:added location page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Saylor\'s Mirissa - Where Ocean Meets Elegance' }}</title>
-    <link href="https://fonts.googleapis.com/css2?family=Belleza&family=STIX+Two+Text:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Belleza&family=STIX+Two+Text:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Sorts+Mill+Goudy&family=NATS&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-['Noto_Sans'] bg-orange-50">
@@ -19,7 +19,7 @@
 
         <!-- Center: Navigation Menu Items -->
         <div class="hidden md:flex justify-center items-center gap-10 mx-auto">
-            <x-mary-menu-item title="HOME" link="#home" class="text-stone-100 text-sm font-normal font-['Noto_Sans'] leading-snug hover:text-stone-900 transition-colors" />
+            <x-mary-menu-item title="HOME" link="/" class="text-stone-100 text-sm font-normal font-['Noto_Sans'] leading-snug hover:text-stone-900 transition-colors" />
             <x-mary-menu-item title="ACCOMMODATION" link="#accommodation" class="text-stone-200 text-sm font-normal font-['Noto_Sans'] leading-snug hover:text-stone-500 transition-colors" />
             <x-mary-menu-item title="LOCATION" link="/location" class="text-stone-200 text-sm font-normal font-['Noto_Sans'] leading-snug hover:text-stone-500 transition-colors" />
             <x-mary-menu-item title="EXPERIENCES" link="#experiences" class="text-stone-200 text-sm font-normal font-['Noto_Sans'] leading-snug hover:text-stone-500 transition-colors" />
